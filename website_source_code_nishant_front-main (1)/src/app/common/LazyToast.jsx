12d799ec119b09b4ab8012b405560b0ac814be2b
unknown <nareshmahi957@gmail.com>
2026-09-03 11:16:23 +0530

    fix 03 morning

--- website_source_code_nishant_front-main (1)/src/app/common/LazyToast.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-export default function LazyToast() {
-  return <ToastContainer />;
-}
--- website_source_code_nishant_front-main (1)/src/app/suhail/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>suhail</div>;
-};
-
-export default page;
